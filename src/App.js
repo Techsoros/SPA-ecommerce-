44d83549efd8c2395a87308df92bc1b082b8c241
Your Name~ <f.zihad333@gmail.com>
2022-10-13 22:15:42 +0600

    Starting practice for 53 no module

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Shop from "./components/Shop/Shop";
-
+// Starting 53 work
 function App() {
   return (
     <div className="my-body">
